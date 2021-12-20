7b77cae813ced69431ed08016b8af9af0c5d902d
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2021-12-20 19:39:38 -0400

    Trabalhando com lifecycle hooks

---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>O meu nome é: {{nome}}</h1>  
+</template>
+
+<script>
+  export default{
+    name: 'LifeCycle',
+    data(){
+      return{
+        nome: 'Ainda não sei'
+      }
+    },
+    created() {
+      setTimeout(() => {
+        this.nome = "Italo"
+      }, 1000);
+    },
+    mounted() { //esta hook tem hierarquia maior que o created
+      setTimeout(() => {
+        this.nome = "Unknown"
+      }, 2000);
+    },
+  }
+</script>
