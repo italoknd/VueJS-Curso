428b7a55ae83ec919356c326139448083245d2dd
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2021-12-25 02:50:27 -0400

    aprendendo o uso de diretivas no vue

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+    <p v-else>Não estou trabalhando</p>
     <p>Utilizo as seguintes tecnologias</p>
     <ul>
       <li>Javascript</li>
@@ -8,11 +9,19 @@
       <li>CSS</li>
       <li>Vue JS</li>
     </ul>
+    <p v-show="mostrar_email">Mande um email para: {{email}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'info',
+  data() {
+    return {
+      esta_trabalhando: true,
+      mostrar_email: false,
+      email: "italopedroza21@gmail.com"
+    }
+  },
 }
 </script>
