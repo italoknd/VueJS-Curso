d932ea0177ad494a623c516a632b416acf497b26
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2021-12-31 19:51:35 -0400

    trabalhando a reutilização de componentes

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -3,11 +3,17 @@
     <h1>Olá Vue</h1>
     <p>Meu nome é {{nome}} e trabalho como {{profissao}}</p>
     <MultiplosEventos/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
   </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue'
+import Reutilizacao from './Reutilizacao.vue'
 
   export default {
     name: 'PrimeiroComponente',
@@ -18,7 +24,8 @@ import MultiplosEventos from './MultiplosEventos.vue'
       }
     },
     components:{
-      MultiplosEventos
+      MultiplosEventos,
+      Reutilizacao
     }
   }
 </script>
