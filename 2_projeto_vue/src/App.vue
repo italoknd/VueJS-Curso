634ccc386eb97211bc15bade31650a0552ce1c12
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-07-30 00:45:06 -0400

    item selecionado por padrão

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,38 +1,65 @@
 <template>
   <div>
-    <Header :esta_logado=true />
-    <PrimeiroComponente/>
-    <LifeCycle/>
-    <Pessoa/>
-    <Form/>
+    <Header :esta_logado="true" />
+    <PrimeiroComponente />
+    <LifeCycle />
+    <Pessoa />
+    <Form />
+
+    <h2>Deixar item selecionado por padrão:</h2>
+    <select v-model="selected">
+      <option
+        v-for="location in locations"
+        :key="location"
+        :value="location.country"
+      >
+        {{ location.country }}
+      </option>
+    </select>
+    <p>Local selecionado: {{ selected }}</p>
   </div>
 </template>
 
 <script>
-  import PrimeiroComponente from './components/PrimeiroComponente.vue'
-  import LifeCycle from './components/LifeCycle.vue'
-  import Pessoa from './components/Pessoa.vue'
-  import Info from './components/Info.vue'
-  import Header from './components/Header.vue'
-  import Form from './components/Form.vue'
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+import Pessoa from './components/Pessoa.vue'
+import Info from './components/Info.vue'
+import Header from './components/Header.vue'
+import Form from './components/Form.vue'
 
-
-  export default {
-    name: 'App',
-    components:{
-      PrimeiroComponente,
-      LifeCycle,
-      Pessoa,
-      Info,
-      Header,
-      Form
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponente,
+    LifeCycle,
+    Pessoa,
+    Info,
+    Header,
+    Form
+  },
+  data() {
+    return {
+      locations: [
+        { country: 'Brazil', id: 1 },
+        { country: 'Germany', id: 2 },
+        { country: 'EUA', id: 3 },
+        { country: 'Spain', id: 4 },
+        { country: 'Netherlands', id: 5 },
+        { country: 'Switzerland', id: 6 }
+      ],
+      selected: ''
     }
+  },
+  created() {
+    this.selected = this.locations[3].country
   }
+}
 </script>
 
 <style>
-  body{
-    background-color: rgb(151, 151, 151);
-    color: white;
-  }
+body {
+  background-color: rgb(151, 151, 151);
+  color: white;
+}
 </style>
