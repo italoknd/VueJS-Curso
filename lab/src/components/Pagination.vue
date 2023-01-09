3227ed60c5f6eb2185369bd521c0c94427e67fb5
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-01-08 21:27:55 -0400

    pagination nativa

--- lab/src/components/Pagination.vue
+++ lab/src/components/Pagination.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div id="paginate">
-      <div class="list">
-        <div v-for="{ item, id } in items" :key="id" class="item">
-          {{ item }}
+      <div class="list w-50 m-auto">
+        <div v-for="{ item, id } in paginatedItems" :key="id" class="item">
+          <p class="text-center">{{ item }}</p>
         </div>
       </div>
-      <nav aria-label="Page navigation example">
+      <nav aria-label="Page navigation example" class="controls mt-5">
         <ul class="pagination">
           <li class="page-item" @click="controls.firstPage()">
             <a class="page-link" aria-label="Previous">
@@ -38,18 +38,34 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 
+onMounted(() => {
+  update()
+})
+
+//data
 const items = Array.from({ length: 100 }).map((_, index) => {
   return { item: `Item ${index + 1}`, id: index + 1 }
 })
 
+//initial state of pagination
 const itemsPerPage = 5
 const state = ref({
   page: 1,
   itemsPerPage,
-  totalPages: Math.ceil(items.length / itemsPerPage) //arredondando para um número maior para evitar perda de itens quando a divisão resulta em números quebrados
+  totalPages: Math.ceil(items.length / itemsPerPage) //rounding to a higher number to avoid lost of items when the division results in broken numbers
 })
+
+const paginatedItems = ref([])
+
+const update = () => {
+  let page = state.value.page - 1
+  let start = page * state.value.itemsPerPage
+  let end = start + state.value.itemsPerPage
+
+  paginatedItems.value = items.slice(start, end)
+}
 
 const controls = ref({
   nextPage() {
@@ -69,4 +85,12 @@ const controls = ref({
     state.value.page = state.value.totalPages
   }
 })
+
+watch(
+  state,
+  () => {
+    update()
+  },
+  { deep: true }
+)
 </script>
