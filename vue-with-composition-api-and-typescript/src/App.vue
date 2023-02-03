da0343c0b17827b27514b2398cf1dcfe5f712104
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-02-03 03:28:37 -0400

    inital commit

---
+++ vue-with-composition-api-and-typescript/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { reactive } from '@vue/reactivity'
+
+const state = reactive({
+  name: 'Link' as string,
+  age: 25 as number | number
+})
+</script>
+
+<template>
+  <div>{{ state }}</div>
+</template>
+
+<style scoped></style>
