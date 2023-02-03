c1bcc0e40fe437e06c07a9a41071e1b3505c8b48
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-02-03 13:37:41 -0400

    mudanças no settings json

--- vue-with-composition-api-and-typescript/src/App.vue
+++ vue-with-composition-api-and-typescript/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { reactive } from '@vue/reactivity'
+import { reactive } from "@vue/reactivity";
 
 const state = reactive({
-  name: 'Link' as string,
+  name: "Link" as string,
   age: 25 as number | number
-})
+});
 </script>
 
 <template>
-  <div>{{ state }}</div>
+  <div>{{ state.name }}</div>
 </template>
 
 <style scoped></style>
