f0b2485f19403d5e815d701f35e47df6e5494e68
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-02-05 11:45:40 -0400

    mudanças...

--- vue-with-composition-api-and-typescript/src/App.vue
+++ vue-with-composition-api-and-typescript/src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-import { reactive } from "@vue/reactivity";
+import { reactive, ref } from "@vue/reactivity";
 
 const state = reactive({
   name: "Link" as string,
   age: 25 as number | number
 });
+
+const end = ref<object>({
+  bairro: "Monte das Oliveiras" as string,
+  numero: 122 as number | string
+});
 </script>
 
 <template>
-  <div>{{ state.name }}</div>
+  <div>
+    <p>{{ state.name }}</p>
+    <p>{{ end.bairro }}</p>
+  </div>
 </template>
 
 <style scoped></style>
