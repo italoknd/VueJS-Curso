4e51cac66c236c175fc1321adb178758cdc273d3
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-05 17:45:22 -0400

    construindo footer

--- vue-studies-and-tests/src/components/Footer.vue
+++ vue-studies-and-tests/src/components/Footer.vue
@@ -1,11 +1,27 @@
 <template>
-<footer class="footer">
-  <ul>
-    <li>Github</li>
-    <li>LnkedIn</li>
-  </ul>
-</footer>
+  <footer class="footer">
+    <ul>
+      <li>Github</li>
+      <li>LinkedIn</li>
+    </ul>
+  </footer>
 </template>
-<script setup lang="ts">
+<script setup lang="ts"></script>
+<style scoped>
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
 
-</script>
+ul > li {
+  margin: 0 10px;
+  transition: 0.3s;
+}
+
+ul > li:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
