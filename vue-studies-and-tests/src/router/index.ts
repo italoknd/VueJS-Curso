75b82feb01a135ad97a5577d479600856976a955
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-05 17:46:27 -0400

    trabalhando com nested routes

--- vue-studies-and-tests/src/router/index.ts
+++ vue-studies-and-tests/src/router/index.ts
@@ -1,16 +1,30 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import User from "../views/User.vue";
+import UserPosts from "../components/User/UserPosts.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/about", component: About },
-  { path: "/contact", component: () => import("../views/Contact.vue") } //using lazy loading  
+  { path: "/contact", component: () => import("../views/Contact.vue") }, //using lazy loading
+  { path: "/products", component: () => import("../views/Products.vue") },
+  {
+    path: "/user/:username",
+    name: "user",
+    component: User,
+    children: [
+      {
+        path: "posts",
+        component: UserPosts
+      }
+    ]
+  }
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
