5c89da293ab23350523b350b15985f721f29493e
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-05 17:47:29 -0400

    criação de seção contact

--- vue-studies-and-tests/src/views/Contact.vue
+++ vue-studies-and-tests/src/views/Contact.vue
@@ -1,9 +1,11 @@
 <template>
-  <h2>
-    Here's the contact page, feel free to get in touch sending a direct message
-    on my
-    <a href="https://www.linkedin.com/in/italo-pedroza/" target="_blank"
-      >LinkedIn</a
-    >:
-  </h2>
+  <div class="main-container">
+    <h2>
+      Here's the contact page, feel free to get in touch sending a direct
+      message on my
+      <a href="https://www.linkedin.com/in/italo-pedroza/" target="_blank"
+        >LinkedIn</a
+      >:
+    </h2>
+  </div>
 </template>
