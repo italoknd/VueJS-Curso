00a0ce6e55243fc9637f0cfb8b7f5335d2d81f1d
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-05 17:46:49 -0400

    criando componente para ser usado no children route

---
+++ vue-studies-and-tests/src/components/User/UserPosts.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { IUserPosts } from "../../interfaces/user";
+
+const posts = ref<IUserPosts[]>([
+  {
+    src: "../../assets/capybara.jpg",
+    caption: "Look to this wonderful capybara!!!",
+    alt: "User post"
+  }
+]);
+</script>
+<template>
+  <section>
+    <img src="../../assets/capybara.jpg" :alt="posts[0].alt" />
+  </section>
+</template>
+<style scoped>
+section{
+height: 100%;
+overflow-y: scroll;
+}
+
+img {
+  height: 400px;
+  width: 400px;
+  object-fit: cover;
+}
+</style>
